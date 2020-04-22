5cb8238309d00080fd0da563bd19c6d858113bc9
alexyu90 <yu90@illinois.edu>
2020-04-22 12:21:24 +0200

    changes to process_data.py train_classifier.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,67 @@
 import sys
-
+import pandas as pd 
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    
+    """
+    This functions loads the messages .csv file and categories .csv file and merge them into one dataframe
+    Input Arguments: messages_filepath, categories_filepath
+    Returns: merged dataframe df
+    """
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories)
+    return df
 
 def clean_data(df):
-    pass
+    
+    """
+    This functions takes the categories column and convert it to 36 seperate columns with binary values
+    and drop duplicates in the end.
+    Input Arguments: original dataframe df
+    Returns: converted dataframe df
+    """
+    
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # use this row to extract a list of new column names for categories.
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.slice(start=-1)
 
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+        
+    # replace the original categories column with the new `categories` dataframe
+    df = df.drop(columns = ['categories'])
+    df = pd.concat([df, categories],axis=1)
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """
+    This functions takes the dataframe and save it to the pointed file path
+    Input Arguments: df, database_filename
+    Returns: none
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('processed', engine, index=False)  
 
-
+    
 def main():
+    """
+    The main function loads, cleans and saves the data to the database
+    """
+    
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,27 +1,92 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+import nltk
+import pickle
 
 def load_data(database_filepath):
-    pass
-
+    """
+    This function loads the dataframe from specified database
+    Input arguments: database_filepath
+    Returns: X, Y, category_names
+    """    
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('processed', engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    category_names= Y.columns
+    
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    """
+    This function tokenizes the given text and returns cleaned and lemmatized tokens
+    Input arguments: text
+    Returns: clean_tokens
+    """  
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 def build_model():
-    pass
-
+    """
+    This function builds a machine learning pipeline
+    Input arguments: none
+    Returns: pipeline
+    """  
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    return pipeline
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    This function test model with test data and prints out classification reports
+    Input arguments: model, X_test, Y_test, category_names
+    Returns: none
+    """  
+    
+    Y_pred = model.predict(X_test)
+    Y_pred_df = pd.DataFrame(Y_pred, columns= category_names)
 
+    for column in Y_pred_df:
+        print("Column: " + column)
+        print(classification_report(Y_test[column], Y_pred_df[column]))
+        print("-"*70)
 
 def save_model(model, model_filepath):
-    pass
-
+    """
+    This function saves the ML model in a pickle file
+    Input arguments: model, model_filepath
+    Returns: none
+    """  
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 def main():
+    """
+    The main function train and saves a ML classifier
+    """  
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
